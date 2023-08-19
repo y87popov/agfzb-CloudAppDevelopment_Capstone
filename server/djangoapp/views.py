ac59749fe27cca39752ec6b5fca1038abef4d432
u87ypopov <87y.popov>
2023-08-19 12:08:42 -0400

    Adding temporary changes to Github

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -74,7 +74,7 @@ def registration_request(request):
     context = {}
     # If it is a GET request, just render the registration page
     if request.method == 'GET':
-        return render(request, 'djangoapp/user_registration.html', context)
+        return render(request, 'djangoapp/registration.html', context)
     # If it is a POST request
     elif request.method == 'POST':
         # Get user information from request.POST
@@ -99,7 +99,7 @@ def registration_request(request):
             login(request, user)
             return redirect("djangoapp:index")
         else:
-            return render(request, 'djangoapp/user_registration.html', context)
+            return render(request, 'djangoapp/registration.html', context)
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     context = {}
