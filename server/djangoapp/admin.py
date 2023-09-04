3433f346691ca747b40704f011ec066e6438eb7c
u87ypopov <87y.popov@gmail.com>
2023-09-04 14:07:27 -0400

    Adding temporary changes to Github

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 # from .models import related models
-
+from .models import CarMake
+from .models import CarModel
 
 # Register your models here.
 
 # CarModelInline class
-
+admin.site.register(CarModelInline)
 # CarModelAdmin class
-
+admin.site.register(CarModelAdmin)
 # CarMakeAdmin class with CarModelInline
-
+admin.site.register(CarMakeAdmin, CarModelInline)
 # Register models here
+admin.site.register(CarMake)
+admin.site.register(CarModel)
