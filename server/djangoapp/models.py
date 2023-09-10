601553f76fd95195d53c16afe57448a3c65413fa
u87ypopov <87y.popov@gmail.com>
2023-09-10 04:38:13 -0400

    Temporary changes.

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -55,6 +55,34 @@ class CarModel(models.Model):
     def __str__(self):
         return "Name: " + self.name
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
 
+    def __init__(_id, _rev, id, city, state, st, address, zip, lat, long, short_name, full_name):
+        # Dealer _id
+        self._id = _id
+        # Dealer _rev
+        self._rev = _rev
+        # Dealer id
+        self.id = id    
+        # Dealer city
+        self.city = city  
+        # Dealer state
+        self.state = state 
+        # Dealer st
+        self.st = st
+        # Dealer address
+        self.address = address
+        # Dealer zip
+        self.zip = zip
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer Full Name
+        self.full_name = full_name
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
