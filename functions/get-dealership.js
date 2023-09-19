e7980ccdb263df291eb0e2c4b07215ad5542b1c7
u87ypopov <87ypopov@gmail.com>
2023-09-19 04:09:46 -0400

    Temporary changes.

---
+++ functions/get-dealership.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const Cloudant = require('@cloudant/cloudant');
+
+// Initialize Cloudant connection with IAM authentication
+async function dbCloudantConnect() {
+    try {
+        const cloudant = Cloudant({
+            plugins: { iamauth: { iamApiKey: 'KIepUqkGJn2eEF2C3USaFGVKMIqYEYUC_ya-tMs6JiAc' } }, // Replace with your IAM API key
+            url: 'https://85b92228-a36b-403b-b65a-acb463cb195c-bluemix.cloudantnosqldb.appdomain.cloud', // Replace with your Cloudant URL
+        });
+
+        const db = cloudant.use('dealerships');
+        console.info('Connect success! Connected to DB');
+        return db;
+    } catch (err) {
+        console.error('Connect failure: ' + err.message + ' for Cloudant DB');
+        throw err;
+    }
+}
+
+let db;
+
+(async () => {
+    db = await dbCloudantConnect();
+})();
+
+app.use(express.json());
+
+// Define a route to get all dealerships with optional state and ID filters
+app.get('/dealerships/get', (req, res) => {
+    const { state, id } = req.query;
+
+    // Create a selector object based on query parameters
+    const selector = {};
+    if (state) {
+        selector.state = state;
+    }
+    if (id) {
+        selector._id = id;
+    }
+
+    const queryOptions = {
+        selector,
+        limit: 10, // Limit the number of documents returned to 10
+    };
+
+    db.find(queryOptions, (err, body) => {
+        if (err) {
+            console.error('Error fetching dealerships:', err);
+            res.status(500).json({ error: 'An error occurred while fetching dealerships.' });
+        } else {
+            const dealerships = body.docs;
+            res.json(dealerships);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
