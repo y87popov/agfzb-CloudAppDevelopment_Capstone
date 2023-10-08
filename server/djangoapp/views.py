d663ca0e1c66f95be90b146f551461f44fe27926
u87ypopov <87y.popov@gmail.com>
2023-10-08 07:40:50 -0400

    Temporary changes(3)

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -106,7 +106,7 @@ def registration_request(request):
 def get_dealerships(request):
     if request.method == "GET":
         context = {}
-        url = "https://u87ypopov-3000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
+        url = "https://u87ypopov-3000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
         dealerships = get_dealers_from_cf(url)
         context["dealership_list"] = dealerships
         return render(request, 'djangoapp/index.html', context)
@@ -116,7 +116,7 @@ def get_dealerships(request):
 def get_dealer_details(request):
     if request.method == "GET":
         context = {}
-        dealer_url = "https://u87ypopov-3000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
+        dealer_url = "https://u87ypopov-3000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
         dealer = get_dealers_from_cf(dealer_url, id=id)
         context["dealer"] = dealer
     
